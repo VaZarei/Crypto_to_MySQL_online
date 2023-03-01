a2397787bfe0ec6002e7d5fd54a056855428ce8f
Vahid Zarei <engitrefinery@gmail.com>
2023-03-01 21:51:17 +0330

    write simple codes for Crud

--- MainCode.py
+++ MainCode.py
@@ -1,20 +1,92 @@
 
-import time
+
+import zeroKey
+import sqlalchemy
+import pandas as pd
+import mysql.connector
+ 
+from sqlalchemy import create_engine, text
+
+from sqlalchemy import insert
+
+
 from Fetch_YF_Functons import *
+from mainView import *
 
 
 
-ticker = "BTC-USD"
+ticker            = initialVar.ticker 
+start_Date        = initialVar.start_Date 
+end_Date          = initialVar.end_Date
+interval          = initialVar.interval
+intervalArray     = initialVar.interval
 
-start_Date = "2010-01-01"  #%Y/%m/%d 
-end_Date = "2023-02-10"
-#end_Date = datetime.now()
+database_username = zeroKey.mySqlConf.get("user")
+database_password = zeroKey.mySqlConf.get("pass")
+database_ip       = '127.0.0.1'
+database_name     = 'traderbot'
 
-interval = "2m"   # 1m # 2m # 5m # 15m # 30m # 60m # 1h # 1d , 5d, 1wk, 1mo, 3m
-intervalArray = ["1m", "2m", "5m", "15m", "30m", "60m", "1h", "1d", "5d", "1wk", "1mo", "3mo", "okok"]
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user=zeroKey.mySqlConf.get("user"),
+  password=zeroKey.mySqlConf.get("pass"),
+  database="traderbot"
+)
+
+
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                                format(database_username, database_password, 
+                                                        database_ip, database_name))
+
+
+
 
 data = fetch_DataF(strTicker=ticker, strStart_Date=start_Date, strEnd_Date=end_Date, strInterval=interval)
-print(data)
 
+table_name="{ticker}_{interval}".format(ticker= ticker.replace("-","") ,interval= interval)
+frame = data.to_sql(con=database_connection, name=table_name , if_exists='replace')
 
+mycursor = mydb.cursor()
+query = """
+        create database if not exists traderbot ;
+        use traderbot ;
+
+        """
+mycursor.execute(query, multi=True)
+mydb.commit()
+
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# update table 
+
+mycursor = mydb.cursor()
+query = "update adausd_1d set Date = '11-01-11' where Date='2023-02-28' ; "
+mycursor.execute(query)
+mydb.commit()
+
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Create or insert  to table 
+
+mycursor = mydb.cursor()
+query = "INSERT INTO adausd_1d (Date, Volume) VALUES (%s , %s)"
+val = ["2024-01-01" , "254789"]
+mycursor.execute(query, val)
+mydb.commit()
+
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Delete from table
+
+mycursor = mydb.cursor()            
+query = " delete from adausd_1d where Date='2023-02-26';"
+mycursor.execute(query)
+mydb.commit()
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Read or select from table
+
+query1 = text("select * from {table_name} where Date > '2023-02-01 12:45:00' ; ".format(table_name=table_name))     
+query1 = text("select * from {table_name}  ; ".format(table_name=table_name))     
+
+df = pd.read_sql(query1, database_connection.connect())
+print(df)
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
 
--- MainCode_T.py
+++ MainCode_T.py
@@ -3,7 +3,7 @@
 import zeroKey
 import sqlalchemy
 import pandas as pd
-import pymysql
+import mysql.connector
  
 from sqlalchemy import create_engine, text
 
@@ -26,30 +26,62 @@ database_password = zeroKey.mySqlConf.get("pass")
 database_ip       = '127.0.0.1'
 database_name     = 'traderbot'
 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user=zeroKey.mySqlConf.get("user"),
+  password=zeroKey.mySqlConf.get("pass"),
+  database="traderbot"
+)
+
+
 database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
                                                 format(database_username, database_password, 
                                                         database_ip, database_name))
 
 
 
+
 data = fetch_DataF(strTicker=ticker, strStart_Date=start_Date, strEnd_Date=end_Date, strInterval=interval)
 
-
-
-query = (("Create DATABASE IF NOT EXISTS traderbot"), ("use traderbot"))
-         
-for i in query :
- print(i)
- database_connection.connect().execute(text(i))
 table_name="{ticker}_{interval}".format(ticker= ticker.replace("-","") ,interval= interval)
 frame = data.to_sql(con=database_connection, name=table_name , if_exists='replace')
-print(database_connection.connect().execute(text("drop DATABASE IF EXISTS bottrad")))
 
+mycursor = mydb.cursor()
+query = """
+        create database if not exists traderbot ;
+        use traderbot ;
 
-
+        """
+mycursor.execute(query, multi=True)
+mydb.commit()
 
 # ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
-# Read or select from database
+# update table 
+
+mycursor = mydb.cursor()
+query = "update adausd_1d set Date = '11-01-11' where Date='2023-02-28' ; "
+mycursor.execute(query)
+mydb.commit()
+
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Create or insert  to table 
+
+mycursor = mydb.cursor()
+query = "INSERT INTO adausd_1d (Date, Volume) VALUES (%s , %s)"
+val = ["2024-01-01" , "254789"]
+mycursor.execute(query, val)
+mydb.commit()
+
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Delete from table
+
+mycursor = mydb.cursor()            
+query = " delete from adausd_1d where Date='2023-02-26';"
+mycursor.execute(query)
+mydb.commit()
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Read or select from table
 
 query1 = text("select * from {table_name} where Date > '2023-02-01 12:45:00' ; ".format(table_name=table_name))     
 query1 = text("select * from {table_name}  ; ".format(table_name=table_name))     
@@ -57,5 +89,4 @@ query1 = text("select * from {table_name}  ; ".format(table_name=table_name))
 df = pd.read_sql(query1, database_connection.connect())
 print(df)
 # ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
-            
 
