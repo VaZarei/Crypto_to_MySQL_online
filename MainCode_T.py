1da43ec772ccea6c3a6f270781f040d866fd6d99
Vahid Zarei <engitrefinery@gmail.com>
2023-03-01 15:16:30 +0330

    Select data from sql and Convert to dataframe ok

--- MainCode_T.py
+++ MainCode_T.py
@@ -7,6 +7,8 @@ import pymysql
  
 from sqlalchemy import create_engine, text
 
+from sqlalchemy import insert
+
 
 from Fetch_YF_Functons import *
 from mainView import *
@@ -30,7 +32,6 @@ database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{
 
 
 
-
 data = fetch_DataF(strTicker=ticker, strStart_Date=start_Date, strEnd_Date=end_Date, strInterval=interval)
 
 
@@ -40,16 +41,21 @@ query = (("Create DATABASE IF NOT EXISTS traderbot"), ("use traderbot"))
 for i in query :
  print(i)
  database_connection.connect().execute(text(i))
-
-
 table_name="{ticker}_{interval}".format(ticker= ticker.replace("-","") ,interval= interval)
 frame = data.to_sql(con=database_connection, name=table_name , if_exists='replace')
-
-
-database_connection.connect().execute(text("drop DATABASE IF EXISTS traderbott"))
+print(database_connection.connect().execute(text("drop DATABASE IF EXISTS bottrad")))
 
 
 
 
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
+# Read or select from database
+
+query1 = text("select * from {table_name} where Date > '2023-02-01 12:45:00' ; ".format(table_name=table_name))     
+query1 = text("select * from {table_name}  ; ".format(table_name=table_name))     
+
+df = pd.read_sql(query1, database_connection.connect())
+print(df)
+# ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
             
 
