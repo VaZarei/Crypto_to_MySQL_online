c12db4425cbeaa99847e2cd122f5153245a7f156
Vahid Zarei <engitrefinery@gmail.com>
2023-03-02 19:44:43 +0330

    make section to backtest and online Condition

--- MainCode_B.py
+++ MainCode_B.py
@@ -1,19 +1,59 @@
-create database if not exists traderbot ;
-use traderbot ;
+from zeroKey import *
+import sqlalchemy
+import pandas as pd
+import mysql.connector
+ 
+from sqlalchemy import create_engine, text
+from Fetch_YF_Functons import *
+from datetime import datetime
 
 
 
- print(" frame :", frame)
+ticker     =  "ada-usd"  # lower case
+start_Date =  "2023-01-01"  #%Y/%m/%d 
+#end_Date  =  "2023-02-10"
+end_Date   =  datetime.now()
+interval   =  "1d"  # ["1m", "2m", "5m", "15m", "30m", "60m", "1h", "1d", "5d", "1wk", "1mo", "3mo"] 
+    
+            
 
-        mycursor = mydb.cursor()
 
-        query = """
-                create database if not exists traderbot ;
-                use traderbot ;
+   
 
-                """
-        mycursor.execute(query, multi=True)
-        mydb.commit()
+
+database_username = mySqlConf("user")
+database_password = mySqlConf("pass")
+database_ip       = '127.0.0.1'
+database_name     = 'traderbot'
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user= mySqlConf("user"),
+  password= mySqlConf("pass"),
+  database="traderbot"
+)
+
+
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                                format(database_username, database_password, 
+                                                        database_ip, database_name))
+
+
+
+
+data = fetch_DataF(strTicker=ticker, strStart_Date=start_Date, strEnd_Date=end_Date, strInterval=interval)
+
+table_name="{ticker}_{interval}".format(ticker= ticker.replace("-","") ,interval= interval)
+frame = data.to_sql(con=database_connection, name=table_name , if_exists='replace')
+
+mycursor = mydb.cursor()
+query = """
+        create database if not exists traderbot ;
+        use traderbot ;
+        """
+mycursor.execute(query, multi=True)
+mydb.commit()
 
 # ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
 # update table 
@@ -45,7 +85,6 @@ mydb.commit()
 query1 = text("select * from {table_name} where Date > '2023-02-01 12:45:00' ; ".format(table_name=table_name))     
 query1 = text("select * from {table_name}  ; ".format(table_name=table_name))     
 
-df = pd.read_sql(query1, database_connection().connect())
+df = pd.read_sql(query1, database_connection.connect())
 print(df)
 # ---------------------------------- ---------------------------------- ---------------------------------- ---------------------------------- ----------------------------------
-
--- zeroKey.py
+++ zeroKey.py
@@ -1,11 +1,52 @@
-mySqlConf = {
 
-    "host" : "localhost",
-    "user" : "admin1" ,
-    "pass" : "admin123" 
 
-}
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import mysql.connector
 
-#print(mySqlConf)
 
-#print(mySqlConf.get("host"))
+
+
+def mySqlConf(value) : 
+
+
+
+        var = {
+
+        "host" : "localhost",
+        "user" : "admin1",
+        "pass" : "admin123",
+        "database_name" : "traderbot" ,
+                                                                      
+         }
+        
+        return var[value]
+
+    
+def database_connection() :
+        
+               
+        database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                                format(mySqlConf("user"), mySqlConf("pass"), 
+                                                        mySqlConf("host"), mySqlConf("database_name")))
+
+        return database_connection
+
+def mydb():
+         
+  mydb = mysql.connector.connect(  
+                                host= mySqlConf("host"),
+                                user= mySqlConf("user"),
+                                password= mySqlConf("pass"),
+                                database=mySqlConf("database_name")
+                                )
+
+  return mydb
+
+#print(type(mySqlConf("user")))
+
+#print(connectionTest())
+
+
+#print(" mydb :", mydb)
+#print(" database_connection :", database_connection)
